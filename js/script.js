759eefb4ecb6d1ea93405bc32f6e0066109c45a9
elchaparr <70244777+elchaparr@users.noreply.github.com>
2024-02-29 16:58:44 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings:['Desarrollador de software', 'Programador'],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
